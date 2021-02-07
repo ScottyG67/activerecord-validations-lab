05efca5c5332c5c7ee0f4ac1e669dd61e45e98cd
github username <scott.gloyna01@gmail.com>
2021-02-06 21:52:52 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates :title, presence: true
+    validates :summary, presence: true, length: {maximum: 250}
+    validates :content, presence: true, length: {minimum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validates_with TitleValidator
+
 end
